6fab2d1e4e3ff7d75a35195e9df90e524440b3e5
MerelMoehn <merelmoehn@outlook.com>
2023-01-30 13:54:16 +0000

    Challenge life cycle

---
+++ my-app/src/components/loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import css from './css/Loader.module.css';
+
+function loader() {
+  return (
+    <div className= {css.Loader}>
+        
+    </div>
+  )
+}
+
+export default loader
