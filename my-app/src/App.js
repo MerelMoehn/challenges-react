15b2f71708080d2f40f4a1255e5d0ecc3c4820b9
MerelMoehn <merelmoehn@outlook.com>
2023-01-30 13:09:35 +0000

    Challenge list iteration

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,17 @@
 import css from './App.module.css';
-import NavBarSimple from './components/NavBarSimple';
-import Sidebar from './components/Sidebar';
+// import NavBarSimple from './components/NavBarSimple';
+// import Sidebar from './components/Sidebar';
+// import NavBarForm from './components/NavBarForm';
+import Content from './components/Content';
 
 
 function App() {
   return (
     <div className={css.App}>
       {/* <Sidebar/>  */}
-      <NavBarSimple />
+      {/* <NavBarSimple /> */}
+      {/* <NavBarForm /> */}
+      <Content />
     </div>
   );
 }
---
+++ my-app/src/components/PostItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import css from "./css/PostItem.module.css";
+
+function PostItem(props) {
+    return (
+        <div>
+                        {
+                            props.savedPosts.map((post) => {
+                                return <div className={css.SearchItem} key={post.title}>
+                                    <p>{post.title}</p>
+                                    <p>{post.name}</p>
+                                    <img src={post.image} alt="random" />
+                                    <p>{post.description}</p>
+                                </div>
+                            })
+                        }
+        </div>
+    )
+}
+
+export default PostItem
